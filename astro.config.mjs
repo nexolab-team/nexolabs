5f825af07463acf207316e18126e879a3f058099
RIchie Havoc <havoc@localhost.localdomain>
2025-11-11 19:21:15 +0300

    Fix: Add correct base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://nexolabs.github.io',
+  site: 'https://nexolab-team.github.io',
+  base: '/nexolabs',
 });
